4263737602f0b1e5b06789e551767022be99f925
liber09 <linda.bergsangel@hotmail.com>
2023-12-20 12:01:44 +0100

    MenuObject child as childcomponent to LandingPage

---
+++ client/packages/pages/landing-page/children/menuObject/ui/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+ import './style.scss';
+
+interface MenuObject {
+    id: string, 
+    title: string,
+    description: string,
+    price: number,
+    category: string
+    ingredients?: [],
+} 
+
+interface MenuObjectProps {
+    data: MenuObject
+}
+
+export const MenuObject= ({data}:MenuObjectProps) => {
+    
+    return (
+        <article>
+            <section>
+                <h3>{data.title}</h3>
+                {<hr className='dottedLine'/>}
+                <h3>{data.price}</h3>
+            </section>
+            <section>
+                <p>{data.description}</p>
+            </section>
+        </article>
+    )
+}
