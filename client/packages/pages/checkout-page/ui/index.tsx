0711a0d1bd865be5bce7199fd49448ad9b03ddc5
liber09 <linda.bergsangel@hotmail.com>
2024-01-03 13:26:44 +0100

    Edded button component to checkout page

--- client/packages/pages/checkout-page/ui/index.tsx
+++ client/packages/pages/checkout-page/ui/index.tsx
@@ -1,9 +1,16 @@
 import './style.scss';
+import { Button } from '@zocom/button';
+
+const handleButtonClick = () => {
+  console.log("Checkout button clicked")
+}
 
 export const CheckoutPage = () => {
     return (
       <main className="checkout-page">
-        
+        <Button type="primary" onClick={handleButtonClick}>
+          Take my money
+        </Button>
       </main>
     );
   };
