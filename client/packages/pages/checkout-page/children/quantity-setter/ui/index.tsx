255e95a7faa423a059206c86a14d6397e23d6bdd
NguyenRobin <robin.nnguyen@hotmail.com>
2024-01-03 09:43:34 +0100

    feature: static cart view

---
+++ client/packages/pages/checkout-page/children/quantity-setter/ui/index.tsx
@@ -0,0 +1,17 @@
+type Props = {
+  quantity: number;
+};
+
+export const QuantitySetter = ({ quantity }: Props) => {
+  return (
+    <section className="quantity-input">
+      <button className="quantity-input__btn-increase">
+        <span className="quantity-input__btn-icon">+</span>
+      </button>
+      <p className="checkout-page__quantity">{quantity} stycken</p>
+      <button className="quantity-input__btn-decrease">
+        <span className="quantity-input__btn-icon">-</span>
+      </button>
+    </section>
+  );
+};
--- client/packages/pages/checkout-page/ui/index.tsx
+++ client/packages/pages/checkout-page/ui/index.tsx
@@ -1,9 +1,83 @@
 import './style.scss';
+import { List, Separator } from '@zocom/list';
+import { Cart } from '@zocom/cart';
+import { Modal } from '@zocom/modal';
+import { ReceiptTotal } from '@zocom/receipt-total';
+import { Button } from '@zocom/button';
+import { QuantitySetter } from '@zocom/quantity-setter';
+
+// Hämta cart
+// Loopa igenom varje item i cart.
+// Rendera det i ul listan.
+// Take my money btn ska skicka order till data basen
+
+const fakeCart = [
+  {
+    quantity: 1,
+    item: 'Karlstad',
+    price: 9,
+  },
+  {
+    quantity: 2,
+    item: 'BANGKOK',
+    price: 9,
+  },
+  {
+    quantity: 1,
+    item: 'Ho Chi Minh',
+    price: 9,
+  },
+  {
+    quantity: 1,
+    item: 'PARIS',
+    price: 9,
+  },
+  {
+    quantity: 1,
+    item: 'Oaxaca',
+    price: 9,
+  },
+  {
+    quantity: 1,
+    item: 'SWEET CHILI DIP',
+    price: 19,
+  },
+];
 
 export const CheckoutPage = () => {
-    return (
-      <main className="checkout-page">
-        
-      </main>
-    );
-  };
+  return (
+    // <Modal>
+    <main className="checkout-page">
+      <section className="checkout-page__cart">
+        <Cart bgColor="transparent" />
+      </section>
+
+      <ul className="checkout-page__customer-cart">
+        {fakeCart.map((item) => {
+          return (
+            <li className="checkout-page__product-description" key={item.item}>
+              <List>
+                <p className="checkout-page__product-name">{item.item}</p>
+                <Separator />
+                <p className="checkout-page__price">{item.price} SEK</p>
+              </List>
+
+              <QuantitySetter quantity={item.quantity} />
+            </li>
+          );
+        })}
+      </ul>
+
+      <section className="checkout-page__checkout">
+        <section className="checkout-page__amount">
+          <ReceiptTotal total={199} />
+        </section>
+
+        <Button onClick={() => console.log('first')} type="primary">
+          Take my Money
+        </Button>
+      </section>
+    </main>
+    // </Modal>
+  );
+};
