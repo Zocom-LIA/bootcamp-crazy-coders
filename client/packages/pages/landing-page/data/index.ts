9671ce7a5323e694dff23fd45f1fb284a23d7039
liber09 <linda.bergsangel@hotmail.com>
2024-01-03 12:30:40 +0100

    cors errors

--- client/packages/pages/landing-page/data/index.ts
+++ client/packages/pages/landing-page/data/index.ts
@@ -2,8 +2,14 @@ export const getMenuData = () => {
   return {
     async fetchMenu() {
       try {
-          const URL = "https://api.jsonbin.io/v3/b/6582ed781f5677401f10d131";
-          const response = await fetch(URL)
+          const URL = "https://m36aao3akl.execute-api.eu-north-1.amazonaws.com/api/menu";
+          const response = await fetch(URL, {
+            method: 'GET', // Specify the HTTP method (GET in this case)
+            headers: {
+              'Content-Type': 'application/json', // Example of a common header
+              'x-api-key': "df6fdea1-10c3-474c-ae62-e63def80de0b", // Add your API key header
+            },
+          });
           const jsonData = await response.json()
           return jsonData;
         } catch (error) {
