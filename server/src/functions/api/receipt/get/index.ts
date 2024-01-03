029281f6cab50a300a25b839c51e2331de389c5e
liber09 <linda.bergsangel@hotmail.com>
2024-01-03 08:36:16 +0100

    added cors

--- server/src/functions/api/receipt/get/index.ts
+++ server/src/functions/api/receipt/get/index.ts
@@ -35,6 +35,10 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
       if (!orderId) {
         return {
           statusCode: 400,
+          headers: {
+            'Access-Control-Allow-Origin': 'http://localhost:5173',
+            'Access-Control-Allow-Credentials': true,
+          },
           body: JSON.stringify({ message: 'Missing orderId in the query parameters' }),
         };
       }
@@ -53,6 +57,10 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
       if (!result) {
         return {
           statusCode: 404,
+          headers: {
+            'Access-Control-Allow-Origin': 'http://localhost:5173',
+            'Access-Control-Allow-Credentials': true,
+          },
           body: JSON.stringify({ message: 'Order not found' }),
         };
       }
@@ -68,6 +76,10 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
       console.error('Error:', error);
       return {
         statusCode: 500,
+        headers: {
+            'Access-Control-Allow-Origin': 'http://localhost:5173',
+            'Access-Control-Allow-Credentials': true,
+          },
         body: JSON.stringify({ message: 'Internal server error:' + error}),
       };
     }
