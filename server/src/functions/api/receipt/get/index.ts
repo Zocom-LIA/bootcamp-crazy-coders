2d3948c07e5fc7a86d6886c635db1d7f20f7e214
liber09 <linda.bergsangel@hotmail.com>
2024-01-03 08:39:11 +0100

    Code cleanup

--- server/src/functions/api/receipt/get/index.ts
+++ server/src/functions/api/receipt/get/index.ts
@@ -2,30 +2,9 @@ import { DynamoDB } from 'aws-sdk';
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 const dynamoDB = new DynamoDB.DocumentClient();
 
-interface LambdaEvent extends APIGatewayProxyEvent {
-  pathParameters: {
-    orderId: string;
-  };
-}
-
-interface IReceiptItem {
-    PK: string;
-    SK: string;
-    orderId: string;
-    customerId: string;
-    selection: {
-      name: string;
-      count: number;
-      totalPrice: number;
-    }[];
-    createdAt: string;
-    totalSum: number;
-  }
-
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
       const orderId: string = event.queryStringParameters?.orderId || '';
-      console.log(orderId);
   
       if (!orderId) {
         return {
