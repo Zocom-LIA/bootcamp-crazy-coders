159676c9f2b81cc4d3dfdb4af6ad9043ffaa7223
liber09 <linda.bergsangel@hotmail.com>
2024-01-04 14:57:38 +0100

    New URL for menu endpoint

--- client/packages/pages/landing-page/data/index.ts
+++ client/packages/pages/landing-page/data/index.ts
@@ -2,7 +2,7 @@ export const getMenuData = () => {
   return {
     async fetchMenu() {
       try {
-          const URL = "https://api.jsonbin.io/v3/b/6582ed781f5677401f10d131";
+          const URL = "https://m36aao3akl.execute-api.eu-north-1.amazonaws.com/api/menu";
           const response = await fetch(URL)
           const jsonData = await response.json()
           return jsonData;
