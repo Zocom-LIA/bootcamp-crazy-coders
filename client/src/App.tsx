6bda8d78b6f213e9312e022ffdbeecec9b03fe38
liber09 <linda.bergsangel@hotmail.com>
2024-01-09 04:53:34 +0100

    recieve message

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,27 @@
 import { AppRoutes } from '@zocom/router';
+import React, { useEffect } from 'react';
+import { initializeApp } from 'firebase/app';
+import { getMessaging, onMessage } from 'firebase/messaging';
+import firebase from 'firebase/app';
+import 'firebase/messaging';
+import firebaseConfig from '../firebaseConfig.ts'
 
-export function App() {
+export const app = firebase.initializeApp(firebaseConfig);
+
+const App: React.FC = () => {
+  useEffect(() => {
+    const messaging = getMessaging(app);
+
+    // Handle messages when the app is in the foreground
+    onMessage(messaging, (payload) => {
+      console.log('Message received:', payload);
+      // Handle the message as needed
+    });
+  }, []);
   return (
     <div className="App">
       <AppRoutes />
     </div>
   );
-}
+};
+export default App;
